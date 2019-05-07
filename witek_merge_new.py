c7a7b0f060ec8056a626a42e9e03ebe9fcce6e9d
pawel <48882583+pawo161@users.noreply.github.com>
2019-05-07 21:07:02 +0200

    Update witek_merge_new.py
    with gif

--- witek_merge_new.py
+++ witek_merge_new.py
@@ -6,7 +6,7 @@ import os
 from PIL import Image
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
-import random
+#import random
 #import keyboard
 import serial
 count = 0
@@ -24,6 +24,7 @@ while True:
             with open("img%s.jpeg" % (count), 'wb') as f:
                 r.raw.decode_content = True
                 shutil.copyfileobj(r.raw, f)
+                print("saved raw")
 
                 #czasomierz
                 #start_time = time.time()
@@ -42,24 +43,31 @@ while True:
                  # doc on shuffle: multi-dimensional arrays are only shuffled along the first axis
                 # so let's make the image an array of (N,3) instead of (m,n,3)
                 rndImg2 = np.reshape(pix, (-1, pix.shape[2]))
-                b = random.randint(1, 5)
-                for k in range(b):
+                #b = random.randint(5,9)
+                images = []
+                for k in range(4):
                     # now shuffle
                     np.random.shuffle(rndImg2)
 
                     # and reshape to original shape
                     rdmImg = np.reshape(rndImg2, pix.shape)
+
                     j = Image.fromarray(rdmImg, mode='RGB')
 
-                    j.save('shuffled/img%sS.jpeg' % (count))
-                    rndImg2 = rdmImg
+                    images.append(j)
+
+                j.save('shuffled/img%sS.gif' % (count), format='GIF', append_images=images[1:], save_all=True, duration=40, loop=0)
+                #rndImg2 = rdmImg
+                print("saved gif")
+
+
 
                 #czasomierz
                 #elapsed_time = time.time() - start_time
                 #print("elapsed time: ", elapsed_time)
                 print(count)
                 if count > 2:
-                    os.remove("shuffled/img%sS.jpeg" % (count-2))
+                    os.remove("shuffled/img%sS.gif" % (count-2))
                     os.remove("img%s.jpeg" % (count-2))
 
 #jak przyśpieszyć ten program? hash? fibonacci?
